210706c0a23d339b5e4e9096737474dbd29d8ee1
Iris Shi <0.0@owo.li>
2025-05-22 14:34:48 +0800

    read file in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,34 @@
-import lpc.logging;
+#include <fstream>
 #include <iostream>
+#include <print>
+#include <string>
+
+import lpc.logging;
 
 using namespace lpc;
 
 auto main(int argc, char* argv[]) -> int {
     try {
-        if (argc == 1)
-            std::cout << "No arguments provided.\n";
-
+        if (argc != 2) {
+            std::cerr << "Usage: " << argv[0] << " <file>\n";
+            return 1;
+        }
         Logger::builder().output(std::cerr).build().make_active();
-        Debug();
-        Info("Error message: ", 0.12f);
-        Warn("Info message: ", reinterpret_cast<void*>(main));
+        // TODO: multiple files
+        if (argc > 2)
+            Warn("Multiple files (arguments) provided, only the first will be used");
+        // TODO: arg parsing
+        std::string file_path = argv[1];
+        std::ifstream input(file_path);
+        if (!input) {
+            Error("Failed to open file: ", file_path);
+            return 1;
+        }
+        std::string content((std::istreambuf_iterator<char>(input)),
+            std::istreambuf_iterator<char>());
+
+        std::print("{}", content);
+
     } catch (const std::exception& e) {
         std::cerr << "Exception: " << e.what() << '\n';
     } catch (...) {
